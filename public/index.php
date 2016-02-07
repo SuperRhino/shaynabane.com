7da630fa0c7d4811d6c4248ab9c4f2465161fb97
Pasto <rpastorelle@yahoo.com>
2016-02-07 11:57:29 -0500

    Break sections into template files.

---
+++ public/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Shayna Bane - Portfolio</title>
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/freelancer.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+    <?php require("./templates/nav.html"); ?>
+    <?php require("./templates/header.html"); ?>
+    <?php require("./templates/portfolio.html"); ?>
+    <?php require("./templates/about.html"); ?>
+    <?php require("./templates/contact.html"); ?>
+    <?php require("./templates/footer.html"); ?>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll visible-xs visible-sm">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+    <!-- Portfolio Modals -->
+    <?php require("./templates/portfolio/modal1.html"); ?>
+    <?php require("./templates/portfolio/modal2.html"); ?>
+    <?php require("./templates/portfolio/modal3.html"); ?>
+    <?php require("./templates/portfolio/modal4.html"); ?>
+    <?php require("./templates/portfolio/modal5.html"); ?>
+    <?php require("./templates/portfolio/modal6.html"); ?>
+
+    <?php require("./templates/scripts.html"); ?>
+</body>
+
+</html>
